b2ed9ec358833220d6b6c002702c4afbb489d44e
Anim52 <dimasplai662@gmail.com>
2025-02-13 16:52:47 +0300

    gg

--- WpfApp1/Views/MainWindow.xaml.cs
+++ WpfApp1/Views/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApp1.ViewModel;
+using WpfApp1.Views.Pages;
 
 namespace WpfApp1.Views
 {
@@ -22,6 +24,38 @@ namespace WpfApp1.Views
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MnViewModel();
+            
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            ContentControlFrame.Content = new AccauntPage();
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            ContentControlFrame.Content = new NumberPage();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            ContentControlFrame = new GuestPage();
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            ContentControlFrame = new EmploeePage();
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            ContentControlFrame = new ServicePage();
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            ContentControlFrame = new ReservationPage();
         }
     }
 }
